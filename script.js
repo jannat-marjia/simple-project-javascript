298d9044dcb24ece81a410701d243cf830f49a2b
jannat-marjia <149683773+jannat-marjia@users.noreply.github.com>
2025-09-28 20:45:15 +0600

    script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert("hello minhaz")
+// console.log()
+// document.write("hello minhaz")
+
+
+let unserOneVaule = prompt("user input data one");
+let userTwoValue =  prompt("user input data two");
+unserOneVaule = parseInt(unserOneVaule);
+unserOneVaule = parseInt(userTwoValue);
+
+const Result = unserOneVaule + userTwoValue;
+const ResultTwo = unserOneVaule - userTwoValue; 
+const ResultThree = unserOneVaule * userTwoValue; 
+const ResultFour = unserOneVaule / userTwoValue;
+const ResultFive = unserOneVaule % userTwoValue; 
+
+document.write("output Add:" + Result + "<br>");
+document.write("output Two Remove:" + ResultTwo +"<br>" );
+document.write("output  Three:" + ResultThree +"<br>"  );
+document.write("output  Four:" + ResultFour +"<br>" );
+document.write("output  Five:" + ResultFive +"<br>" );
+
+
+ 
